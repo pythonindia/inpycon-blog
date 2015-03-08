3c9ec04c4ce871834c8c0be019ede221dbfebdf5
Vignesh Sarma K <vignesh@recruiterbox.com>
2015-03-08 13:15:44 +0530

    A minimal pelican setup

---
+++ content/blog/test.rst
@@ -0,0 +1,7 @@
+Test Article
+###############
+
+:Date: 2015-3-03 10:20
+:Category: Test
+
+This can be removed as soon as more, real articles are added.
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'PSSI'
+SITENAME = u'In PyCon Blog'
+SITEURL = ''
+
+PATH = 'content'
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+ARTICLE_PATHS = ['blog']
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('InPyCon', 'http://in.pycon.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('PSSI', 'http://pssi.org.in/'),)
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
