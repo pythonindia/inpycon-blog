f6c1b14871bb02a394a940846768dedba5118099
Vijay Kumar B <vijaykumar@zilogic.com>
2019-05-01 21:25:33 +0530

    Added initial PyCon India 2019 theme, and made it the default.

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,7 +21,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 ARTICLE_PATHS = ['blog']
-THEME = 'themes/inpycon2018'
+THEME = 'themes/inpycon2019'
 
 # Blogroll
 LINKS = (('InPyCon', 'http://in.pycon.org/'),
